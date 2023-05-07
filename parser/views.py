a049a4e80a3d5468d637c7c50b12e040b4501e4b
mnmustafa1109 <mnmustafa1109@gmail.com>
2023-05-08 01:39:02 +0500

    added time printing and calculation of cycles time

--- parser/views.py
+++ parser/views.py
@@ -33,7 +33,7 @@ def result(request):
     tasks = []
     gateways = []
     flows = []
-
+    
     # go into bpmn:definitions child 
     for child in myroot:
         # find number of bpmn:process child
@@ -109,31 +109,31 @@ def result(request):
         for child in process:
             if child.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}sequenceFlow":
                 flows.append(child)
-            if child.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}messageFlow":
-                flows.append(child)
-            if child.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}association":
-                flows.append(child)
-            if child.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}dataAssociation":
-                flows.append(child)
-            if child.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}dataInputAssociation":
-                flows.append(child)
-            if child.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}dataOutputAssociation":
-                flows.append(child)
             
 
 
 
     template_name = "index.html"
     
+    
+    processes_data = {}
+    
+    for process in processlist:
+        processes_data[process.attrib['name']] = 0
+    
     # store name of the lanes, tasks, events and processes in a list
     
-    lanes_name = []
+    lanes_data = {}
     
     for lane in lanelist:
-        lanes_name.append(lane.attrib['name'])
+        lanes_data[lane.attrib['name']] = 0
         
     task_data = {}
     total_time = 0
+    
+    event_data = {}
+    
+    cycle_time = 0.0
     
         # go into extensionElements tag
     for task in tasks:
@@ -141,37 +141,100 @@ def result(request):
             if child.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}extensionElements":
                 for child2 in child:
                     if child2.tag == "{http://camunda.org/schema/zeebe/1.0}properties":
-                        print(child2.tag)
+                        current_time = 0.0
+                        current_probability = 1.0
                         for child3 in child2:
                             if child3.tag == "{http://camunda.org/schema/zeebe/1.0}property":
-                                    # get the Time property 
+                                # check if it have a Probability property
+                                if child3.attrib['name'] == "Probability":
+                                    current_probability = float(child3.attrib['value'])
+                                # get the Time property 
                                 if child3.attrib['name'] == "Time":
                                     task_data[task.attrib['name']] = child3.attrib['value']
                                     total_time += int(child3.attrib['value'])
-
+                                    current_time = int(child3.attrib['value'])
+                                    #check if the task is in a lane
+                                    for lane in lanelist:
+                                        for child4 in lane:
+                                            if child4.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}flowNodeRef":
+                                                if child4.text == task.attrib['id']:
+                                                    lanes_data[lane.attrib['name']] += int(child3.attrib['value'])
+                                    # check if the task is in a process
+                                    for process in processlist:
+                                        for child4 in process:
+                                            if child4.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}task":
+                                                if child4.attrib['id'] == task.attrib['id']:
+                                                    processes_data[process.attrib['name']] += int(child3.attrib['value'])
+                        cycle_time += current_time * current_probability
+                            
+                                
+    print("Total time: " + str(total_time))
         
-    events_name = []
-    
     for event in events:
-        events_name.append(event.attrib['name'])
-        
-    processes_name = []
+        event_data[event.attrib['name']] = 0
+        for child in event:
+            if child.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}extensionElements":
+                for child2 in child:
+                    if child2.tag == "{http://camunda.org/schema/zeebe/1.0}properties":
+                        current_time = 0.0
+                        current_probability = 1.0
+                        for child3 in child2:
+                            if child3.tag == "{http://camunda.org/schema/zeebe/1.0}property":
+                                # check if it have a Probability property
+                                if child3.attrib['name'] == "Probability":
+                                    current_probability = float(child3.attrib['value'])
+                                # get the Time property 
+                                if child3.attrib['name'] == "Time":
+                                    event_data[event.attrib['name']] = child3.attrib['value']
+                                    current_time = int(child3.attrib['value'])
+                                    total_time += int(child3.attrib['value'])
+                                    #check if the task is in a lane
+                                    for lane in lanelist:
+                                        for child4 in lane:
+                                            if child4.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}flowNodeRef":
+                                                if child4.text == event.attrib['id']:
+                                                    lanes_data[lane.attrib['name']] += int(child3.attrib['value'])
+                                    # check if the task is in a process
+                                    for process in processlist:
+                                        for child4 in process:
+                                            if child4.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}event":
+                                                if child4.attrib['id'] == event.attrib['id']:
+                                                    processes_data[process.attrib['name']] += int(child3.attrib['value'])
+                        cycle_time += current_time * current_probability
+                        
     
-    for process in processlist:
-        processes_name.append(process.attrib['name'])
         
     gateways_name = []
     
     for gateway in gateways:
         gateways_name.append(gateway.attrib['name'])
         
+   
+    sum_events = 0
+    sum_tasks = 0
+    sum_lanes = 0
+    sum_processes = 0
+    
+    for event in event_data:
+        sum_events += int(event_data[event])
+    
+    for task in task_data:
+        sum_tasks += int(task_data[task])
+        
+    for lane in lanes_data:
+        sum_lanes += int(lanes_data[lane])
+        
+    for process in processes_data:
+        sum_processes += int(processes_data[process])
+        
+    
 
     # remove file
     # if os.path.isfile("static/upload/bpmn.xml"):
         # os.remove("static/upload/bpmn.xml")
     
     # display the numbers of lanes, tasks, events and processes
-    return render(request, template_name, {'lanes': lanes_name, 'tasks': task_data, 'events': events_name, 'processes': processes_name, 'gateways': gateways_name, 'flows': flows, 'total_time': total_time})
+    return render(request, template_name, {'lanes': lanes_data, 'tasks': task_data, 'events': event_data, 'processes': processes_data, 'gateways': gateways_name, 'flows': flows, 'total_time': total_time, 'sum_events': sum_events, 'sum_tasks': sum_tasks, 'sum_lanes': sum_lanes, 'sum_processes': sum_processes, 'cycle_time': cycle_time})
 
     
     
