a2a04d56388b94cbbb2db2afde268b10f32b7f26
mnmustafa1109 <mnmustafa1109@gmail.com>
2023-05-09 20:44:39 +0500

    formatted code

--- parser/views.py
+++ parser/views.py
@@ -1,30 +1,27 @@
 from django.views.generic import TemplateView
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.core.files import File
 from django.core.files.storage import FileSystemStorage
 import xml.etree.ElementTree as ET
-import os 
-from . import forms 
-
+import os
+from . import forms
 
 xmlpath = "static/upload/bpmn.xml"
+
 
 def readfile(request):
     f = open(xmlpath, "r")
     if f.mode == 'r':
-       contents =f.read()
+        contents = f.read()
     return contents
 
 
 def result(request):
-    
-    #return error if file is not present
+    # return error if file is not present
     if not os.path.exists(xmlpath):
         return upload(request, error="File not found")
-        
-    
-    
+
     myroot = ET.fromstring(readfile(request))
 
     lanelist = []
@@ -33,7 +30,7 @@ def result(request):
     tasks = []
     gateways = []
     flows = []
-    
+
     # go into bpmn:definitions child 
     for child in myroot:
         # find number of bpmn:process child
@@ -41,17 +38,12 @@ def result(request):
             # save the all process child in a list
             processlist.append(child)
 
-    
-
-
     for process in processlist:
         for child in process:
             if child.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}laneSet":
                 laneSet = child
                 for lane in laneSet:
                     lanelist.append(lane)
-
-
 
     for process in processlist:
         for child in process:
@@ -65,7 +57,6 @@ def result(request):
                 events.append(child)
             if child.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}boundaryEvent":
                 events.append(child)
-            
 
     for process in processlist:
         for child in process:
@@ -104,40 +95,36 @@ def result(request):
                 gateways.append(child)
             if child.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}complexGateway":
                 gateways.append(child)
-                
+
     for process in processlist:
         for child in process:
             if child.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}sequenceFlow":
                 flows.append(child)
-            
-
-
 
     template_name = "index.html"
-    
-    
+
     processes_data = {}
-    
+
     for process in processlist:
         processes_data[process.attrib['name']] = 0
-    
+
     # store name of the lanes, tasks, events and processes in a list
-    
+
     lanes_data = {}
-    
+
     for lane in lanelist:
         lanes_data[lane.attrib['name']] = 0
-        
+
     task_data = {}
     total_time = 0
-    
+
     event_data = {}
-    
+
     cycle_time = 0.0
-    
-        # go into extensionElements tag
+
+    # go into extensionElements tag
     for task in tasks:
-        for child in task:    
+        for child in task:
             if child.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}extensionElements":
                 for child2 in child:
                     if child2.tag == "{http://camunda.org/schema/zeebe/1.0}properties":
@@ -153,7 +140,7 @@ def result(request):
                                     task_data[task.attrib['name']] = child3.attrib['value']
                                     total_time += int(child3.attrib['value'])
                                     current_time = int(child3.attrib['value'])
-                                    #check if the task is in a lane
+                                    # check if the task is in a lane
                                     for lane in lanelist:
                                         for child4 in lane:
                                             if child4.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}flowNodeRef":
@@ -164,12 +151,12 @@ def result(request):
                                         for child4 in process:
                                             if child4.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}task":
                                                 if child4.attrib['id'] == task.attrib['id']:
-                                                    processes_data[process.attrib['name']] += int(child3.attrib['value'])
+                                                    processes_data[process.attrib['name']] += int(
+                                                        child3.attrib['value'])
                         cycle_time += current_time * current_probability
-                            
-                                
+
     print("Total time: " + str(total_time))
-        
+
     for event in events:
         event_data[event.attrib['name']] = 0
         for child in event:
@@ -188,7 +175,7 @@ def result(request):
                                     event_data[event.attrib['name']] = child3.attrib['value']
                                     current_time = int(child3.attrib['value'])
                                     total_time += int(child3.attrib['value'])
-                                    #check if the task is in a lane
+                                    # check if the task is in a lane
                                     for lane in lanelist:
                                         for child4 in lane:
                                             if child4.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}flowNodeRef":
@@ -199,47 +186,46 @@ def result(request):
                                         for child4 in process:
                                             if child4.tag == "{http://www.omg.org/spec/BPMN/20100524/MODEL}event":
                                                 if child4.attrib['id'] == event.attrib['id']:
-                                                    processes_data[process.attrib['name']] += int(child3.attrib['value'])
+                                                    processes_data[process.attrib['name']] += int(
+                                                        child3.attrib['value'])
                         cycle_time += current_time * current_probability
-                        
-    
-        
+
     gateways_name = []
-    
+
     for gateway in gateways:
         gateways_name.append(gateway.attrib['name'])
-        
-   
+
     sum_events = 0
     sum_tasks = 0
     sum_lanes = 0
     sum_processes = 0
-    
+
     for event in event_data:
         sum_events += int(event_data[event])
-    
+
     for task in task_data:
         sum_tasks += int(task_data[task])
-        
+
     for lane in lanes_data:
         sum_lanes += int(lanes_data[lane])
-        
+
     for process in processes_data:
         sum_processes += int(processes_data[process])
-        
-    
 
     # remove file
     # if os.path.isfile("static/upload/bpmn.xml"):
-        # os.remove("static/upload/bpmn.xml")
-    
-    # display the numbers of lanes, tasks, events and processes
-    return render(request, template_name, {'lanes': lanes_data, 'tasks': task_data, 'events': event_data, 'processes': processes_data, 'gateways': gateways_name, 'flows': flows, 'total_time': total_time, 'sum_events': sum_events, 'sum_tasks': sum_tasks, 'sum_lanes': sum_lanes, 'sum_processes': sum_processes, 'cycle_time': cycle_time})
+    # os.remove("static/upload/bpmn.xml")
 
-    
-    
+    # display the numbers of lanes, tasks, events and processes
+    return render(request, template_name,
+                  {'lanes': lanes_data, 'tasks': task_data, 'events': event_data, 'processes': processes_data,
+                   'gateways': gateways_name, 'flows': flows, 'total_time': total_time, 'sum_events': sum_events,
+                   'sum_tasks': sum_tasks, 'sum_lanes': sum_lanes, 'sum_processes': sum_processes,
+                   'cycle_time': cycle_time})
+
+
 # upload file
-def upload(request,error = None):
+def upload(request, error=None):
     template_name = "upload.html"
     if request.method == "POST":
         form = forms.UploadFileForm(request.POST, request.FILES)
@@ -251,10 +237,9 @@ def upload(request,error = None):
             filename = fs.save("static/upload/bpmn.xml", request.FILES['file'])
             uploaded_file_url = fs.url(filename)
             return redirect('result')
-            
-            
+
+
     else:
-        form = forms.UploadFileForm()        
+        form = forms.UploadFileForm()
 
     return render(request, template_name, {'form': form, 'error': error})
-    
