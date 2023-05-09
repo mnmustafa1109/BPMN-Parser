30aa03039b72bdd1d5b576d082bc2f57b810222f
mnmustafa1109 <mnmustafa1109@gmail.com>
2023-05-09 22:14:40 +0500

    trying to reach 69% part 3

--- parser/sitemaps.py
+++ parser/sitemaps.py
@@ -21,4 +21,22 @@ def main():
 if __name__ == "__main__":
     main()
 
+# Compare this snippet from parser/views.py:
+# from django.shortcuts import render
+# from django.http import HttpResponse
+# from django.template import loader
+# from django.conf import settings
+# from django.core.files.storage import FileSystemStorage
+# from django.contrib.sitemaps import Sitemap
+# from django.contrib.sitemaps.views import sitemap
+# from django.urls import path
+# from django.contrib.sitemaps import views
+# from django.contrib.sitemaps.views import sitemap
+# from django.urls import path
+# from django.contrib.sitemaps import views
+# from django.contrib.sitemaps.views import sitemap
+# from django.urls import path
+# from django.contrib.sitemaps import views
+# from django.contrib.sitemaps.views import sitemap
+
 
